c06745dcec65812e6b2be1d2cafda18a8c700701
Varshith K <varshithknaik@gmail.com>
2025-11-14 23:43:10 +0530

    refactor: restructure index.js to implement clustering and improve performance

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const express = require('express');
+process.env.UV_THREADPOOL_SIZE = 2;
 
-const app = express();
+const crypto = require("crypto");
 
-app.get('/' , (req , res) => {
-  res.send("Hi There")
-})
+const clustor = require("cluster");
 
-app.listen(3000);
+// Is the file being executed in master mode?
+if (clustor.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  clustor.fork();
+  // clustor.fork();
+  // clustor.fork();
+  // clustor.fork();
+} else {
+  // Im a child, Im going to act lke a server and do nothing else
+  const express = require("express");
+  const app = express();
+
+  app.get("/", (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send("Hi There");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3000);
+}
