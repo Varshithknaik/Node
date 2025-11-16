084fd33808dc0670075290424070182493143959
Varshith K <varshithknaik@gmail.com>
2025-11-16 18:54:29 +0530

    Refactor code structure for improved readability and maintainability

--- AdvancedNodeStarter/index.js
+++ AdvancedNodeStarter/index.js
@@ -1,21 +1,45 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 
-import App from './components/App';
-import reducers from './reducers';
+require('./models/User');
+require('./models/Blog');
+require('./services/passport');
 
-import axios from 'axios';
-window.axios = axios;
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const app = express();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
+
+if (['production'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
