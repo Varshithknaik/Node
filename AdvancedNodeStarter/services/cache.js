21f21543f1d187a4f0e16bcf4430321efefc6bd6
Varshith K <varshithknaik@gmail.com>
2025-11-24 23:18:15 +0530

    feat: implement caching mechanism for blog queries using Redis

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -4,12 +4,23 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 
 const exec = mongoose.Query.prototype.exec;
 
+
+mongoose.Query.prototype.cache = function(options = {}){
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+}
+
+
 mongoose.Query.prototype.exec = async function (...args) {
+  if(!this.useCache) {
+    return exec.apply(this, args);
+  }
 
   // Copying the functions that we won't modify the function call
   const key = JSON.stringify(Object.assign({} , this.getQuery() , {
@@ -17,7 +28,7 @@ mongoose.Query.prototype.exec = async function (...args) {
   }))
 
   // See if we have a value for 'key' in redis;
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey ,key);
 
   // If we do, return that
   if(cacheValue){
@@ -32,7 +43,7 @@ mongoose.Query.prototype.exec = async function (...args) {
   // Otherwisw, issue the query abs store the result in redis;
 
   const result = await exec.apply(this, args);
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey,key, JSON.stringify(result), 'EX', 10);
   console.log('SERVING FROM DATABASE')
   return result;
 
